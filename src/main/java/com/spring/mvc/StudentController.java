ed97e39f67a7087a6a8065d8a7db66d1438239b4
vaishnavikachole <vaishnavik2016@gmail.com>
2022-07-26 20:53:33 +0530

    spring mvc model and view

--- src/main/java/com/spring/mvc/StudentController.java
+++ src/main/java/com/spring/mvc/StudentController.java
@@ -2,15 +2,26 @@ package com.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class StudentController {
 	
 	@RequestMapping("/students")
-	public String getStudents()
+	public 	ModelAndView getStudents()
 	{
 		System.out.println("this is student controller");
-		return "student";
+		
+		//creating object
+		ModelAndView modelandview = new ModelAndView();
+		
+		//setting data
+		modelandview.addObject("name","shifali");
+		modelandview.addObject("rollnumber",12);
+		
+		
+		modelandview.setViewName("student");
+		return modelandview;
 	}
 	
 	
--- src/main/webapp/WEB-INF/pages/student.jsp
+++ src/main/webapp/WEB-INF/pages/student.jsp
@@ -8,8 +8,15 @@ pageEncoding="ISO-8859-1"%>
     <title>Student</title>
 </head>
 <body>
-    <h1>Student Data</h1>
+    <%
+     String name = (String) request.getAttribute("name");
+     Integer rollnumber = (Integer) request.getAttribute("rollnumber");
     
+     
+    %>
+    <h1>student name is <%= name%> </h1>
+    
+    <h1>student roll_number is <%= rollnumber%> </h1>
     
 </body>
 </html>
